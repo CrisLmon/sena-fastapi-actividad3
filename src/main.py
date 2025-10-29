182b398d07c1c8f84ffb5cb88af87a2e669b6981
jdmor-prog <jdmoreno26@outlook.com>
2025-10-28 23:31:49 -0500

    feat: definir endpoint config

--- src/main.py
+++ src/main.py
@@ -28,6 +28,13 @@ def obtener_item(
     
     return { "item_id": item_id, "q": q, "activo": activo}
 
+@app.get("/config")
+def config(
+    modo: str = "produccion",
+    version: float = 1.0
+):
+    return {"modo": modo, "version": version}
+
 # uvicorn main:app --reload
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
